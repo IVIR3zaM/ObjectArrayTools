23a2e57db0379d8734ba8dad0c3a4c3807716873
Mohammad Reza Maghoul <m.reza.maghool@gmail.com>
2016-12-24 14:23:18 +0330

    remove garbages

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,14 +7,3 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 } else {
     throw new \Exception('autoload file not found');
 }
-use IVIR3aM\ObjectArrayTools\AbstractActiveArray;
-
-class ActiveArray extends AbstractActiveArray {}
-
-$object = new ActiveArray(['How', 'Are', 'You']);
-
-print_r($object->getData());
-
-$object->sort();
-
-print_r($object->getData());exit;
